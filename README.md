94c9fa84dd70fae48c561d4c729dfcede66ce967
MatejMaricic <matej.maricic@gmail.com>
2019-02-02 10:00:54 +0100

    latest version of app, no changes yet, testing gitignore

--- README.md
+++
@@ -1,1 +0,0 @@
-# InchooPHPAkademijaZ6
---
+++ app/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'url' => 'http://phpakademija.loc/InchooPHPAkademijaZ6/',
+    'mode'  => 'development',
+    "db"=>[
+        "host"=>"localhost",
+        "name"=>"social_network",
+        "user"=>"root",
+        "password"=>"admin123"
+    ]
+];
